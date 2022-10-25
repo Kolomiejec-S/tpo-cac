ba29da6f77d10f1c513be29ed40f6245f9e4212f
lilfracasado <santiagomkmj@gmail.com>
2022-10-24 21:18:45 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // NAVBAR RESPONSIVE
-checkbtn = document.querySelector(".checkbtn");
-checkbtn.onclick = function() {
-    navBar = document.querySelector(".navbar-left");
-    navBar.classList.toggle("active");
-}
+checkbtn = document.querySelector('.checkbtn');
+checkbtn.onclick = function () {
+  navBar = document.querySelector('.navbar-left');
+  navBar.classList.toggle('active');
+};
